953cc13b25ef802f0b958f5bfc8d3e7f8910af0e
Johnson Joseph <jhonsonj88@gmail.com>
2019-03-17 23:54:59 -0700

    Update cli.rb

--- lib/fandango/cli.rb
+++ lib/fandango/cli.rb
@@ -1,41 +1,47 @@
-#Our CLI Controller 
-
 class Fandango::CLI
     
     def call 
-        puts "Welcome to Fandango! Any night is a movie night!"
+      Fandango::Movies.scrape_movies
+        welcome 
         list_movies
         menu 
         goodbye
     end 
+    
+    def welcome
+       puts "Welcome to Fandango! Any night is a movie night!"
+    end 
 
 
     def list_movies
-        @showings = Fandango::Movies.showings
-        @showings.each.with_index(1) do |movies, i|
-            puts "#{i}. #{movies.name} - #{movies.price} - #{movies.availability}"
-        end 
-    end 
+      Movie.all.each.with_index(1) do |movie, i|
+        puts "#{i}. #{movie.name}"
+    end
+  end 
 
 
     def menu
         input = nil 
         while input != "exit"
-        puts "Enter the number of the movie you'd would like to see or type list to see the movies again or type exit:"
+        puts "Enter the number of the movie you'd would like to see or, type list to see the movies again or type exit:"
         input = gets.strip.downcase
 
-        if input.to_i > 0 
-        the_movies =  @showings[input.to_i-1]
-        puts "#{the_movies.name} - #{the_movies.price} - #{the_movies.availability}"
+        if input.to_i > 0 && input.to_i < 10
+        the_movies =  list_movies[input.to_i-1]
+        puts "#{the_movies.name}"
         elsif input == "list"
             list_movies
+        elsif input != "exit"
+          puts "Invalid response."
+          
+          binding.pry
     else 
         puts "Not sure what you want, type list or exit."
-    end 
+      end 
   end 
 end  
 
     def goodbye
         puts "See you tomorrow for a list of new movies!!"
     end 
-end  
+  end 
