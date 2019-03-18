07f5bbc190947c0123bbe90a786bf4f70004ce15
Johnson Joseph <jhonsonj88@gmail.com>
2019-03-17 17:29:31 -0700

    final stage

--- lib/fandango/movies.rb
+++ lib/fandango/movies.rb
@@ -1,32 +1,42 @@
-class Fandango::Movies 
+require_relative "./version"
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
+class Fandango::Movie 
 
 attr_accessor :name, :price, :availability, :url 
 
     def self.showings
-       # I should return a bunch of instances of Movies 
-       #Scape data and return movies based on data
+       self.scrape_movies_data
+        #[movie_1, movie_2]
+    end 
     
-        movie_1 = self.new
-        movie_1.name = "What Men Want"
-        movie_1.price = $15
-        movie_1.availability = true
-        movie_1.url = "https://www.fandango.com/"
-
-
-        movie_2 = self.new
-        movie_2.name = "The Upside"
-        movie_2.price = $15
-        movie_2.availability = true
-        movie_2.url = "https://www.fandango.com/"
-
-        [movie_1, movie_2]
-
+    def self.scrape_movies_data
+       movies = []
+       
+       movies << self.scrape_movies
+       
+       movies 
+      
     end 
 
-    def self.scape_movies 
-        movies = []
-        #go to Fandango.com, find the movies
-        #extract the properties
-        #instantiate a deal 
+    def self.scrape_movies 
+       
+        doc = Nokogiri::HTML(open("https://www.fandango.com/"))
+        doc1 = Nokogiri::HTML(open("https://tickets.fandango.com/transaction/ticketing/express/ticketboxoffice.aspx?row_count=266828305&tid=AAWTJ&sdate=2019-03-17+16:45&mid=188283&from=mov_det_showtimes"))
+        
+            
+            movie = self.new
+            movie.name = doc.search("a.heading-style-1.heading-size-s.heading__movie-carousel")[0...2]
+            movie.name.each do |movie|
+                 puts movie.text
+                 puts movie.attr("href") #url 
+            movie.price = doc1.search("td.pricePerTicket")[0].text 
+            movie.availability = true 
+            
+            movie
+   binding.pry
+          end
+       
     end 
-end     
+end 
