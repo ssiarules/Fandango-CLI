6386a1fbaf467be2d069ae472e0fee1a9fa21c55
Johnson Joseph <jhonsonj88@gmail.com>
2019-03-17 21:20:26 -0700

    Only scrapping movie names

--- lib/fandango/movies.rb
+++ lib/fandango/movies.rb
@@ -2,41 +2,27 @@ require_relative "./version"
 require 'nokogiri'
 require 'open-uri'
 require 'pry'
-class Fandango::Movie 
+class Fandango::Movies 
 
-attr_accessor :name, :price, :availability, :url 
+attr_accessor :name
 
-    def self.showings
-       self.scrape_movies_data
-        #[movie_1, movie_2]
-    end 
+  def self.showings
+    movies = []
     
-    def self.scrape_movies_data
-       movies = []
-       
-       movies << self.scrape_movies
-       
-       movies 
-      
-    end 
+    movies << self.scrape_movies
+  end 
 
     def self.scrape_movies 
-       
-        doc = Nokogiri::HTML(open("https://www.fandango.com/"))
-        doc1 = Nokogiri::HTML(open("https://tickets.fandango.com/transaction/ticketing/express/ticketboxoffice.aspx?row_count=266828305&tid=AAWTJ&sdate=2019-03-17+16:45&mid=188283&from=mov_det_showtimes"))
+         doc = Nokogiri::HTML(open("https://www.fandango.com/"))
         
-            
-            movie = self.new
-            movie.name = doc.search("a.heading-style-1.heading-size-s.heading__movie-carousel")[0...2]
-            movie.name.each do |movie|
-                 puts movie.text
-                 puts movie.attr("href") #url 
-            movie.price = doc1.search("td.pricePerTicket")[0].text 
-            movie.availability = true 
-            
-            movie
-   binding.pry
+           movies = self.new
+           movies.name = doc.search("a.heading-style-1.heading-size-s.heading__movie-carousel")[0...3]
+           movies.name.each do |movie|
+            puts movie.text.strip
+            #puts movie.attr("href")
+           
+           movies 
+   #binding.pry
           end
-       
+       end 
     end 
-end 
